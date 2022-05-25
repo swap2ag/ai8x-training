99b1c167b79e072e4fac76e6cf358908b763c3b4
Swapnil <agarwal.swapnil640@gmail.com>
2022-05-25 21:36:06 +0000

    fix run script

--- start_training.sh
+++ start_training.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
+ROOT_DIR=/content
+TRAIN_REPO_DIR=$ROOT_DIR/ai8x-training
+SYNTHESIS_REPO_DIR=$ROOT_DIR/ai8x-synthesis
+PYTHON_VERSION=python3.8
+
 # start training
 cd $TRAIN_REPO_DIR
 source $TRAIN_REPO_DIR/venv-train/bin/activate
-chmod +x ./scripts/train_mnist.sh
-./scripts/train_mnist.sh
+chmod +x ./scripts/train_mnist_extrasmall.sh
+./scripts/train_mnist_extrasmall.sh
